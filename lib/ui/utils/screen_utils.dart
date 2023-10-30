d6e9e644ad5c58a09816f28e0b3794a5a2f6407b
romulo <romulo.inbox@gmail.com>
2023-10-30 19:54:04 -0400

    Updated ScreenUtils to use mobile size.

--- lib/ui/utils/screen_utils.dart
+++ lib/ui/utils/screen_utils.dart
@@ -1,8 +1,10 @@
 class ScreenUtils {
   static double calculateGridSize(double screenWidth) {
-    if (screenWidth > 1150) {
+    if (screenWidth > 1200) {
       return 5;
-    } else if (screenWidth > 680) {
+    } else if (screenWidth > 900) {
+      return 4;
+    } else if (screenWidth > 600) {
       return 3;
     } else {
       return 2;
