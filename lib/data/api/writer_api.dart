c21f0956902a0db7df7ff8dcd1b41f006f7dd21d
romulo <romulo.inbox@gmail.com>
2024-02-07 08:14:21 -0300

    Peformance improvements - Added Isolate calls to parse json from external apis.

--- lib/data/api/writer_api.dart
+++ lib/data/api/writer_api.dart
@@ -1,12 +1,16 @@
+import 'dart:isolate';
+
 import 'package:bookclub/data/samples/writer_samples.dart';
 import 'package:bookclub/domain/entities/writer.dart';
 
 class WriterApi {
   Future<List<Writer>> getWriters() {
     return Future.delayed(const Duration(seconds: 1), () {
-      return WriterSamples.getSamples().map((e) {
-        return Writer.fromMap(e);
-      }).toList();
+      return Isolate.run(() {
+        return WriterSamples.getSamples().map((e) {
+          return Writer.fromMap(e);
+        }).toList();
+      });
     });
   }
 }
